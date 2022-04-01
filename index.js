c0495f5432e038c5432b599a2c0168d0502c34d3
Agasthian <agasthian_r@hotmail.com>
2022-04-01 10:32:21 +0530

    Added Mongoose lib & connected with MonogoDb Cloud atlas

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 require('./services/passport')
 // const authRoutes = require('./routes/authRoute')
+const mongoose = require("mongoose")
+const keys = require('./config/keys')
 
+
+
+/** Connect Mongo DB */
+mongoose.connect(keys.mongoURI)
+
+/** Express App start */
 const app = express();
 // authRoutes(app); //Refactor the code
 require('./routes/authRoute')(app)
