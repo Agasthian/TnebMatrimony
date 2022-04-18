7e1dca59d90507b10c6677de9d8e682e1560b7f3
Agasthian <agasthian_r@hotmail.com>
2022-04-18 18:18:44 +0530

    Added Development and production credentials for hosting

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// Keys.js
+if(process.env.NODE_ENV === 'production'){
+    //We are in production - return prod set of keys
+    module.exports=require('./prod')
+} else {
+    //We are in development - return dev keys
+    module.exports=require('./dev')
+}
--- services/passport.js
+++ services/passport.js
@@ -27,6 +27,7 @@ passport.use(new GoogleStrategy({
     clientID : keys.googleClientID,
     clientSecret : keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
+    proxy: true,
 
 }, (accessToken,refreshToken,profile, done)=> {
 
