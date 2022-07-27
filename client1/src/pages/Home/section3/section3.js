f21cd8eec6f93e0de26c37c7b4c258bb19281b62
Agasthian <agasthian_r@hotmail.com>
2022-07-27 22:29:41 +0530

    Home page Design compeleted

--- client1/src/pages/Home/section3/section3.js
+++ client1/src/pages/Home/section3/section3.js
@@ -9,14 +9,16 @@ const Section3 = () => {
           
         </div>
         <div className="section3_wrapper-content">
-          <h3>Why Choose TNEB Matimony</h3>
+          <h3>TNEB Matimony finds the perfect one </h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo rerum excepturi natus dignissimos nulla distinctio sed dolores culpa numquam, illo error similique consequatur ut ullam quos soluta eaque porro dolor.</p>
           <br />
+          <h4>Why Choose TNEB Matimony</h4>
           <ul>
             <li>Trusted service for all</li>
             <li>Verified phone numbers</li>
             <li>Large number of profiles</li>
           </ul>
+          <button className='btn btn--orange'>Get Started - Register now</button>
         </div>
       </div>
     </div>
